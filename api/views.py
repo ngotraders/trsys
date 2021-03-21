b046c7cf7a7417b9a0b8cc1e42cb3104f2fb53d0
Yuto Nagano <chamelion@hotmail.co.jp>
2021-03-21 23:58:23 +0900

    ローカルメモリーのキャッシュを導入

--- api/views.py
+++ api/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import get_object_or_404
-from django.http.request import HttpHeaders
-from django.http.response import HttpResponseRedirect
-from django.http import Http404, HttpResponse
-from django.urls import reverse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.core.cache import cache
 
 from .models import Order, OrderType
 
@@ -11,20 +8,26 @@ from .models import Order, OrderType
 @csrf_exempt
 def order_index(request):
     if (request.method == 'GET'):
-        orders = Order.objects.all()
+        orders = cache.get('orders')
+        if not orders:
+            orders = Order.objects.all()
+            cache.set('orders', orders)
+
         response = ''
         for order in orders:
             if response:
                 response += '@'
             response += f'{order.ticket_no}:{order.symbol}:{order.order_type}'
         return HttpResponse(response)
-
-    Order.objects.all().delete()
-    for item in request.body.decode().split('@'):
-        ticket_no, symbol, order_type = item.split(':')
-        Order(
-            ticket_no=ticket_no,
-            symbol=symbol,
-            order_type=OrderType[order_type],
-        ).save()
-    return HttpResponse(status=200)
+    elif request.method == 'POST':
+        Order.objects.all().delete()
+        for item in request.body.decode().split('@'):
+            ticket_no, symbol, order_type = item.split(':')
+            order = Order(
+                ticket_no=ticket_no,
+                symbol=symbol,
+                order_type=OrderType[order_type],
+            )
+            order.save()
+        cache.delete('orders')
+        return HttpResponse(status=200)
--- trsys/settings.py
+++ trsys/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
 }
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
