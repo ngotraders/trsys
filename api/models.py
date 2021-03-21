103c9fdba7ac59ceed2039ea374e8d3cd5166e49
Yuto Nagano <chamelion@hotmail.co.jp>
2021-03-22 00:06:43 +0900

    modelsに文字列表現を設定

--- api/models.py
+++ api/models.py
@@ -10,3 +10,6 @@ class Order(models.Model):
     ticket_no = models.TextField()
     symbol = models.TextField()
     order_type = models.TextField(choices=OrderType.choices)
+
+    def __str__(self) -> str:
+        return f'{self.ticket_no}:{self.symbol}:{self.order_type}'
