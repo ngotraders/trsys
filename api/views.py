0bc2fb6ea649f35ae673804944fa96c13b6eb404
Yuto Nagano <chamelion@hotmail.co.jp>
2021-03-22 13:53:10 +0900

    キャッシュを改めて無効化

--- api/views.py
+++ api/views.py
@@ -1,20 +1,14 @@
 from django.http import HttpResponse
 from django.http.response import HttpResponseBadRequest
-from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Order, OrderType
-
-CACHE_KEY_ORDERS = 'orders'
 
 
 @csrf_exempt
 def order_index(request):
     if (request.method == 'GET'):
-        orders = cache.get(CACHE_KEY_ORDERS)
-        if not orders:
-            orders = Order.objects.all()
-            cache.set(CACHE_KEY_ORDERS, orders)
+        orders = Order.objects.all()
         response = ''
         for order in orders:
             if response:
@@ -38,5 +32,4 @@ def order_index(request):
                 order_type=OrderType(order_type),
             )
             order.save()
-        cache.set(CACHE_KEY_ORDERS, Order.objects.all())
         return HttpResponse(status=200)
