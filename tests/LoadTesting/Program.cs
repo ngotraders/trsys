bab734b02d8b3cec3cf37962d7bf1b4d5fd66659
永野ゆうと <chamelion@hotmail.co.jp>
2021-03-29 11:51:16 +0900

    NBomberのプロジェクトを作成

---
+++ tests/LoadTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿using NBomber.Contracts;
+using NBomber.CSharp;
+using System.Threading.Tasks;
+
+namespace LoadTesting
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var step = Step.Create("step", async context =>
+            {
+                await Task.Delay(1);
+                return Response.Ok();
+            });
+
+            var scenario = ScenarioBuilder.CreateScenario("hello_world", step);
+
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .Run();
+        }
+    }
+}
