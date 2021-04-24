f0c9c20e942d613b56cb900bf5aae33a2b860331
永野ゆうと <chamelion@hotmail.co.jp>
2021-04-24 16:30:08 +0900

    mql4のパスを変更

--- scripts/build-mql4.bat
+++ scripts/build-mql4.bat
@@ -1,7 +1,7 @@
 SET PATH=C:\Program Files (x86)\FXCM MetaTrader4;%PATH
 pushd %~dp0
-metaeditor.exe /compile:"..\mql\TrsysPublisher.mq4"
+metaeditor.exe /compile:"..\mql4\TrsysPublisher.mq4"
 move ..\mql\TrsysPublisher.ex4 ..\static\downloads
-metaeditor.exe /compile:"..\mql\TrsysSubscriber.mq4"
+metaeditor.exe /compile:"..\mql4\TrsysSubscriber.mq4"
 move ..\mql\TrsysSubscriber.ex4 ..\static\downloads
 popd
