606969e758b844159eebb62849631c7a1a0136f1
Yuto Nagano <chamelion@hotmail.co.jp>
2021-05-11 00:08:54 +0900

    インデックスを設定

--- src/Trsys.Web/Data/TrsysContext.cs
+++ src/Trsys.Web/Data/TrsysContext.cs
@@ -16,5 +16,18 @@ namespace Trsys.Web.Data
         public DbSet<SecretKey> SecretKeys { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<SecretKey>()
+                .Property(s => s.Key)
+                .HasMaxLength(256)
+                .IsRequired();
+            modelBuilder.Entity<SecretKey>()
+                .HasIndex(s => s.Key);
+            modelBuilder.Entity<Event>()
+                .HasIndex(e => e.Timestamp);
+        }
     }
 }
