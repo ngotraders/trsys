f24dd5a25e9919074a569ddb2b95f79ec558d71c
Yuto Nagano <chamelion@hotmail.co.jp>
2021-05-11 23:14:45 +0900

    イベントにソースフィールドを追加

--- src/Trsys.Web/Services/EventService.cs
+++ src/Trsys.Web/Services/EventService.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Trsys.Web.Models.Events;
 
@@ -24,42 +22,17 @@ namespace Trsys.Web.Services
 
         public Task RegisterSystemEventAsync(string eventType, object data = null)
         {
-            return submitter.SendAsync(new Event()
-            {
-                EventType = $"system/{eventType}",
-                Data = SerializeData(data),
-            });
+            return submitter.SendAsync(Event.Create("system", eventType, data));
         }
 
         public Task RegisterEaEventAsync(string secretKey, string eventType, object data = null)
         {
-            return submitter.SendAsync(new Event()
-            {
-                EventType = $"ea/{secretKey}/{eventType}",
-                Data = SerializeData(data),
-            });
+            return submitter.SendAsync(Event.Create($"ea/{secretKey}", eventType, data));
         }
 
         public Task RegisterUserEventAsync(string username, string eventType, object data = null)
         {
-            return submitter.SendAsync(new Event()
-            {
-                EventType = $"user/{username}/{eventType}",
-                Data = SerializeData(data),
-            });
-        }
-
-        private string SerializeData(object data)
-        {
-            if (data == null)
-            {
-                return null;
-            }
-            else if (data.GetType().IsPrimitive || data is string || data is decimal)
-            {
-                return data.ToString();
-            }
-            return JsonConvert.SerializeObject(data);
+            return submitter.SendAsync(Event.Create($"user/{username}", eventType, data));
         }
     }
 }
