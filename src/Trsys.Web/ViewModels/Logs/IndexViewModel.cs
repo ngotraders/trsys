1d0bb9e9d3d07bdac95f94d508eae9de3489e443
Yuto Nagano <chamelion@hotmail.co.jp>
2021-06-06 11:12:06 +0900

    Logsのフォルダが無視されている不具合を修正

---
+++ src/Trsys.Web/ViewModels/Logs/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Trsys.Web.Models.ReadModel.Dtos;
+
+namespace Trsys.Web.ViewModels.Logs
+{
+    public class IndexViewModel
+    {
+        public int Page { get; set; }
+        public int PerPage { get; set; }
+        public List<LogDto> Events { get; set; }
+
+        public string Source { get; set; }
+    }
+}
