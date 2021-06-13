74b6ed69f982bba185be05755bd034511f20d421
Yuto Nagano <chamelion@hotmail.co.jp>
2021-06-13 18:42:36 +0900

    サーバーの改修に合わせてヘッダー等を変更

--- locustfile.py
+++ locustfile.py
@@ -4,6 +4,7 @@ from urllib3.exceptions import InsecureRequestWarning
 
 urllib3.disable_warnings(InsecureRequestWarning)
 import uuid
+import time
 
 
 class Admin:
@@ -21,24 +22,27 @@ class Admin:
                                     'Password': password
                                 })
 
-    def createKey(self, key: str, key_type: str):
-        return self.client.post("/admin/keys/new",
-                                data={
-                                    'Key': key,
-                                    'KeyType': key_type
-                                })
+    def createKey(self, key: str):
+        return self.client.post("/api/keys", json={'Key': key, 'KeyType': 3})
 
     def activateKey(self, key: str):
-        return self.client.post(f"/admin/keys/{key}/approve",
-                                name="/admin/keys/[key]/approve")
+        return self.client.put(f"/api/keys/{key}",
+                               name='/api/keys/[key]',
+                               json={
+                                   'KeyType': 3,
+                                   'IsApproved': True,
+                               })
 
     def deactivateKey(self, key: str):
-        return self.client.post(f"/admin/keys/{key}/revoke",
-                                name="/admin/keys/[key]/revoke")
+        return self.client.put(f"/api/keys/{key}",
+                               name='/api/keys/[key]',
+                               json={
+                                   'KeyType': 3,
+                                   'IsApproved': False,
+                               })
 
     def deleteKey(self, key: str):
-        return self.client.post(f"/admin/keys/{key}/delete",
-                                name="/admin/keys/[key]/delete")
+        return self.client.delete(f"/api/keys/{key}", name="/api/keys/[key]")
 
 
 class Subscriber(HttpUser):
@@ -53,7 +57,9 @@ class Subscriber(HttpUser):
     def fetch_secret_key(self):
         self.client.headers = {}
         self.client.headers['Content-Type'] = 'text/plain'
-        self.client.headers['X-Ea-Version'] = '20210331'
+        self.client.headers['X-Ea-Id'] = self.secretKey
+        self.client.headers['X-Ea-Type'] = 'Subscriber'
+        self.client.headers['X-Ea-Version'] = '20210609:Locust'
         res = self.client.post("/api/token", data=self.secretKey)
         if res.status_code == 200:
             self.token = res.text
@@ -62,7 +68,7 @@ class Subscriber(HttpUser):
         self.secretKey = str(uuid.uuid4())
         admin = Admin(self.client)
         admin.login("admin", "P@ssw0rd")
-        admin.createKey(self.secretKey, "3")
+        admin.createKey(self.secretKey)
         admin.activateKey(self.secretKey)
 
     def on_stop(self):
@@ -81,7 +87,9 @@ class Subscriber(HttpUser):
             self.client.cookies.clear()
             self.client.headers = {}
             self.client.headers['Content-Type'] = 'text/plain'
-            self.client.headers['X-Ea-Version'] = '20210331'
+            self.client.headers['X-Ea-Id'] = self.secretKey
+            self.client.headers['X-Ea-Type'] = 'Subscriber'
+            self.client.headers['X-Ea-Version'] = '20210609:Locust'
             self.client.headers['X-Secret-Token'] = self.token
 
         if self.etag != '':
@@ -91,7 +99,9 @@ class Subscriber(HttpUser):
             self.etag = res.headers['ETag']
             self.client.post(
                 '/api/logs',
-                data=f'{self.secretKey}/{self.token}/{res.content}')
+                data=
+                f'{int(time.time())}:DEBUG:{self.secretKey}/{self.token}/{res.content}'
+            )
         elif res.status_code == 401:
             self.token = ''
         elif res.status_code != 304:
