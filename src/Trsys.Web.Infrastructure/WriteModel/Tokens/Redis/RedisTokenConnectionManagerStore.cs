b0cbc6274ce057db8ce219b53ec21efcbffe136c
Yuto Nagano <chamelion@hotmail.co.jp>
2021-06-19 10:01:20 +0900

    Redisのアクセスを1秒に1回に制限

--- src/Trsys.Web.Infrastructure/WriteModel/Tokens/Redis/RedisTokenConnectionManagerStore.cs
+++ src/Trsys.Web.Infrastructure/WriteModel/Tokens/Redis/RedisTokenConnectionManagerStore.cs
@@ -1,5 +1,6 @@
 using StackExchange.Redis;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,10 +10,12 @@ namespace Trsys.Web.Infrastructure.WriteModel.Tokens.Redis
 {
     public class RedisTokenConnectionManagerStore : ITokenConnectionManagerStore
     {
+        private readonly ConcurrentDictionary<string, DateTimeOffset> lastAccessed = new();
         private readonly IConnectionMultiplexer connection;
         private static readonly RedisKey tokenKey = RedisHelper.GetKey("TokenConnectionManagerStore:Tokens");
         private static readonly RedisKey lastAccessedKey = RedisHelper.GetKey("TokenConnectionManagerStore:LastAccessed");
         private static readonly TimeSpan fiveSeconds = TimeSpan.FromSeconds(5);
+        private static readonly TimeSpan oneSecond = TimeSpan.FromSeconds(1);
 
         public RedisTokenConnectionManagerStore(IConnectionMultiplexer connection)
         {
@@ -38,11 +41,20 @@ namespace Trsys.Web.Infrastructure.WriteModel.Tokens.Redis
 
         public async Task<(bool, Guid)> ExtendTokenExpirationTimeAsync(string token)
         {
+            var now = DateTimeOffset.UtcNow;
+            if (!lastAccessed.TryGetValue(token, out var timestamp))
+            {
+                lastAccessed.TryAdd(token, now);
+            }
+            if (now - timestamp < oneSecond)
+            {
+                return (false, Guid.Empty);
+            }
             var cache = connection.GetDatabase();
             var value = await cache.HashGetAsync(tokenKey, token);
             if (value.HasValue)
             {
-                if (await cache.SortedSetAddAsync(lastAccessedKey, token, (DateTimeOffset.UtcNow + fiveSeconds).ToUnixTimeSeconds(), When.Always))
+                if (await cache.SortedSetAddAsync(lastAccessedKey, token, (now + fiveSeconds).ToUnixTimeSeconds(), When.Always))
                 {
                     return (true, Guid.Parse(value.ToString()));
                 }
@@ -53,6 +65,7 @@ namespace Trsys.Web.Infrastructure.WriteModel.Tokens.Redis
 
         public async Task<(bool, Guid)> ClearExpirationTimeAsync(string token)
         {
+            lastAccessed.TryRemove(token, out var _);
             var cache = connection.GetDatabase();
             var value = await cache.HashGetAsync(tokenKey, token);
             if (value.HasValue)
