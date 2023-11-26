0ed09437c4f56c6ebe6b0dd96d0267a29b6b666c
Yuto Nagano <chamelion@hotmail.co.jp>
2023-11-26 05:12:49 +0000

    メール送信処理を実装

--- src/Trsys.Infrastructure/ReadModel/UserNotification/EmailSender.cs
+++ src/Trsys.Infrastructure/ReadModel/UserNotification/EmailSender.cs
@@ -1,4 +1,9 @@
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 
 namespace Trsys.Infrastructure.ReadModel.UserNotification
 {
@@ -9,18 +14,47 @@ namespace Trsys.Infrastructure.ReadModel.UserNotification
 
     public class EmailSender : IEmailSender
     {
+        private readonly EmailSenderConfiguration configuration;
+
         public EmailSender(EmailSenderConfiguration configuration)
         {
+            this.configuration = configuration;
         }
 
-        public Task SendEmailAsync(string emailAddress, string subject, string body)
+        public async Task SendEmailAsync(string emailAddress, string subject, string body)
         {
-            return Task.CompletedTask;
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(configuration.MailFrom, configuration.MailFrom));
+            message.To.Add(new MailboxAddress(emailAddress, emailAddress));
+            message.Subject = subject;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(configuration.Host, configuration.Port, configuration.UseSsl);
+
+                if (!string.IsNullOrEmpty(configuration.AuthenticationUser))
+                {
+                    client.Authenticate(configuration.AuthenticationUser, configuration.AuthenticationPassword);
+                }
+
+                await client.SendAsync(message);
+                client.Disconnect(true);
+            }
         }
     }
 
     public class EmailSenderConfiguration
     {
         public string Host { get; set; }
+        public int Port { get; set; }
+        public bool UseSsl { get; set; }
+        public string AuthenticationUser { get; set; }
+        public string AuthenticationPassword { get; set; }
+        public string MailFrom { get; set; }
     }
 }
---
+++ tests/Trsys.Infrastructure.Tests/EmailSenderTests.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trsys.Infrastructure.ReadModel.UserNotification;
+
+namespace Trsys.Infrastructure.Tests
+{
+    [TestClass]
+    [Ignore]
+    public class EmailSenderTests
+    {
+        [TestMethod]
+        public async Task When_sending_email_then_mail_sent()
+        {
+            var configuration = new EmailSenderConfiguration
+            {
+                Host = "smtp",
+                Port = 1025,
+                UseSsl = false,
+                // AuthenticationUser = "user",
+                // AuthenticationPassword = "password",
+                MailFrom = "test@smtp",
+            };
+            var sut = new EmailSender(configuration);
+            await sut.SendEmailAsync("target@smtp", "subject", "body");
+        }
+    }
+}
