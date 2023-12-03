3fe398fc6a33db3524416e7aa9d2481ba9486e0f
Yuto Nagano <chamelion@hotmail.co.jp>
2023-12-03 19:53:03 +0900

    設定を修正

--- tests/Trsys.Infrastructure.Tests/MailKitEmailSenderTests.cs
+++ tests/Trsys.Infrastructure.Tests/MailKitEmailSenderTests.cs
@@ -6,6 +6,7 @@ using Trsys.Infrastructure.ReadModel.UserNotification;
 namespace Trsys.Infrastructure.Tests
 {
     [TestClass]
+    [Ignore]
     public class MailKitEmailSenderTests
     {
         [TestMethod]
@@ -13,23 +14,14 @@ namespace Trsys.Infrastructure.Tests
         {
             var configuration = new EmailSenderConfiguration
             {
-                Host = "smtp.office365.com",
-                Port = 587,
-                UseSsl = true,
-                // AuthenticationUser = "copy-trading-system@xsys.co.jp",
-                // AuthenticationPassword = "Xuv15129",
-                // AuthenticationUser = "yuto.nagano@xsys.co.jp",
-                // AuthenticationPassword = "Gs9q4if9",
-                AuthenticationClientId = "3dca753e-90d3-4840-915f-a7521490e7cf",
-                AuthenticationAuthority = "https://login.microsoftonline.com/748076ca-4ade-484a-8c5d-7186653aad71/v2.0",
-                AuthenticationClientSecret = "ADG8Q~K5T1p4jjG8o2aPR9yX4WqbthhcxryUMcVg",
-                AuthenticationScopes = new List<string> { "https://outlook.office365.com/.default" },
-                AuthenticationUser = "copy-trading-system@xsys.co.jp",
-                MailFrom = "copy-trading-system@xsys.co.jp",
+                Host = "localhost",
+                Port = 1025,
+                UseSsl = false,
+                MailFrom = "copy-trading-system@example.com",
             };
 
             var sut = new MailKitEmailSender(configuration);
-            await sut.SendEmailAsync("yuto.nagano@xsys.co.jp", "subject", "body");
+            await sut.SendEmailAsync("copy-trading-system@example.com", "subject", "body");
         }
     }
 }
