57bf54117ffc98ba769324c829616767c6ef2da3
Yuto Nagano <chamelion@hotmail.co.jp>
2023-12-19 17:37:21 +0000

    トレードの画面を一部最適化

--- frontend/src/pages/trade-histories/list.tsx
+++ frontend/src/pages/trade-histories/list.tsx
@@ -1,6 +1,92 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { MuiListInferencer } from "@refinedev/inferencer/mui";
+import React from "react";
+import {
+  useDataGrid,
+  EditButton,
+  ShowButton,
+  DeleteButton,
+  List,
+  DateField,
+} from "@refinedev/mui";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { IResourceComponentsProps, useTranslate } from "@refinedev/core";
+import { Typography } from "@mui/material";
 
 export const TradeHistoryList: React.FC<IResourceComponentsProps> = () => {
-  return <MuiListInferencer />;
+  const translate = useTranslate();
+  const { dataGridProps } = useDataGrid();
+
+  const columns = React.useMemo<GridColDef[]>(
+    () => [
+      {
+        field: "publisherId",
+        flex: 1,
+        headerName: translate("trade-histories.fields.publisherId"),
+        minWidth: 300,
+      },
+      {
+        field: "ticketNo",
+        flex: 1,
+        headerName: translate("trade-histories.fields.ticketNo"),
+        type: "number",
+        minWidth: 150,
+      },
+      {
+        field: "symbol",
+        flex: 1,
+        headerName: translate("trade-histories.fields.symbol"),
+        minWidth: 150,
+      },
+      {
+        field: "orderType",
+        flex: 1,
+        headerName: translate("trade-histories.fields.orderType"),
+        minWidth: 150,
+        renderCell: function render({ value }) {
+          return translate(`trade-histories.orderTypes.${value}`);
+        },
+      },
+      {
+        field: "openPublishedAt",
+        flex: 1,
+        headerName: translate("trade-histories.fields.openPublishedAt"),
+        minWidth: 250,
+        renderCell: function render({ value }) {
+          return <DateField value={value} format="YYYY-MM-DD HH:mm:ss" />;
+        },
+      },
+      {
+        field: "closePublishedAt",
+        flex: 1,
+        headerName: translate("trade-histories.fields.closePublishedAt"),
+        minWidth: 250,
+        renderCell: function render({ value }) {
+          return <DateField value={value} format="YYYY-MM-DD HH:mm:ss" />;
+        },
+      },
+      {
+        field: "actions",
+        headerName: translate("table.actions"),
+        sortable: false,
+        renderCell: function render({ row }) {
+          return (
+            <>
+              <EditButton hideText recordItemId={row.id} />
+              <ShowButton hideText recordItemId={row.id} />
+              <DeleteButton hideText recordItemId={row.id} />
+            </>
+          );
+        },
+        align: "center",
+        headerAlign: "center",
+        minWidth: 80,
+      },
+    ],
+    [translate]
+  );
+
+  return (
+    <List>
+      <DataGrid {...dataGridProps} columns={columns} autoHeight />
+    </List>
+  );
 };
--- frontend/src/pages/trade-histories/show.tsx
+++ frontend/src/pages/trade-histories/show.tsx
@@ -1,6 +1,67 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { MuiShowInferencer } from "@refinedev/inferencer/mui";
+import {
+  useShow,
+  IResourceComponentsProps,
+  useTranslate,
+} from "@refinedev/core";
+import {
+  Show,
+  TextFieldComponent as TextField,
+  NumberField,
+  DateField,
+} from "@refinedev/mui";
+import { Typography, Stack } from "@mui/material";
 
 export const TradeHistoryShow: React.FC<IResourceComponentsProps> = () => {
-  return <MuiShowInferencer />;
+  const translate = useTranslate();
+  const { queryResult } = useShow();
+  const { data, isLoading } = queryResult;
+
+  const record = data?.data;
+
+  return (
+    <Show isLoading={isLoading}>
+      <Stack gap={1}>
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.ticketNo")}
+        </Typography>
+        <NumberField value={record?.ticketNo ?? ""} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.symbol")}
+        </Typography>
+        <TextField value={record?.symbol} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.orderType")}
+        </Typography>
+        <NumberField value={record?.orderType ?? ""} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.priceOpened")}
+        </Typography>
+        <NumberField value={record?.priceOpened ?? ""} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.timeOpened")}
+        </Typography>
+        <DateField value={record?.timeOpened} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.priceClosed")}
+        </Typography>
+        <NumberField value={record?.priceClosed ?? ""} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.timeClosed")}
+        </Typography>
+        <DateField value={record?.timeClosed} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.percentage")}
+        </Typography>
+        <NumberField value={record?.percentage ?? ""} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.openPublishedAt")}
+        </Typography>
+        <DateField value={record?.openPublishedAt} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("trade-histories.fields.closePublishedAt")}
+        </Typography>
+        <DateField value={record?.closePublishedAt} />
+      </Stack>
+    </Show>
+  );
 };
